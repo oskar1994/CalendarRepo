46c2b0345bd703bee7657e53ccced393208e953c
oskar1994 <oskar.strz@gmail.com>
2017-04-16 19:26:11 +0200

    zaimplementowanie modelu bazy danych, stworzenie bazy za pomocą Entity Framework, M klasa notatek

--- CalendarSolution/Model/Note.cs
+++ CalendarSolution/Model/Note.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq.Mapping;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CalendarSolution.Model
 {
+    [Table(Name= "CalendarDb")]
     public class Note
     {
-        public Note()
-        {
-            Id = Guid.NewGuid();
-        }
-        public Guid Id { get; set; }
-        public string Content;
-        public DateTime Date;
-
+        [Column(Name = "Id", IsPrimaryKey = true, CanBeNull = false)]
+        public int Id { get; set; }
+        [Column(CanBeNull = true)]
+        public string Content { get; set; }
+        [Column(CanBeNull = false)]
+        public DateTime Date { get; set; }
     }
 }
