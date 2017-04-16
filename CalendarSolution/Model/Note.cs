3fb50b7c4c25d6041498226363de5a56034fc8b5
oskar1994 <oskar.strz@gmail.com>
2017-04-16 14:50:03 +0200

    Wprowadzenie klasy modelu Note.cs

---
+++ CalendarSolution/Model/Note.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarSolution.Model
+{
+    public class Note
+    {
+        public Note()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string Content;
+        public DateTime Date;
+
+    }
+}
