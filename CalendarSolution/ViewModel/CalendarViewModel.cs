fbaa015b7edbae3c399f19682078ef394efce967
Noxam <NoxamSherei@gmail.com>
2017-04-17 14:41:50 +0200

    -Stworzenie Przycisku Poprzedni -Stworzenie Przycisku Nastepny -Stworzenie Kalendarza -Stworzenie TextBlocku wyświetlania Daty -Binding dla Przycisku Poprzedni -Binding dla przycisku Nastepny -Binding dla Kalendarza -Binding dla TextBloku wyświetlania Daty -Stworzenie Klasy CalendardViewModel -Stworzenie Publicznego Eventu PropertyChangedEventHandler PropertyChanged -Podpięcie Metody OnPropertyChanged -Przypisanie Wyrównania w Pliku XAML dla TextBlock i Kalendarza oraz StackPanel -Wyłączenie Opcji zmiany wielkości okna -Dodanie Pola dla Daty -Stworzenie Własciwosci dla Daty -Podpięcie Właściwości pod Datę

---
+++ CalendarSolution/ViewModel/CalendarViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarSolution.ViewModel
+{
+    class CalendarViewModel : INotifyPropertyChanged
+    {
+        #region Field
+        public event PropertyChangedEventHandler PropertyChanged; 
+        private DateTime _date; //Pole zawierające Datę
+        #endregion
+
+
+        #region Methods
+        public DateTime Date //Publiczna właściwość Date dla _date
+        {
+            get { return _date; }
+            set
+            {
+                if (value != null && value != _date)
+                {
+                    _date = value;
+                    OnPropertyChanged("Date");
+                }
+            }
+        }
+
+        protected void OnPropertyChanged(string Label)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(Label));
+            }
+        }
+        #endregion
+    }
+}
