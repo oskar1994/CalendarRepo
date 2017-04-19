4c4330f2cc90ce4a414d77550f8c219ba8ca1970
Jakub <kubekpl1@wp.pl>
2017-04-19 20:39:36 +0200

    NextButton

--- CalendarSolution/ViewModel/CalendarViewModel.cs
+++ CalendarSolution/ViewModel/CalendarViewModel.cs
@@ -4,16 +4,27 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
-namespace CalendarSolution.ViewModel
+namespace CalendarSolution.ViewModel 
 {
     class CalendarViewModel : INotifyPropertyChanged
     {
-        #region Field
-        public event PropertyChangedEventHandler PropertyChanged; 
-        private DateTime _date; //Pole zawierające Datę
-        #endregion
+        
+        private ICommand nextButtonCommand;
+        private DateTime _date;
 
+        private void NextButton()
+        { Date = Date.AddDays(1); }
+        
+        public ICommand NextButtonCommand
+        {
+            get
+            { nextButtonCommand = new RelayCommand<object>((x) => NextButton());
+            return nextButtonCommand; }
+            }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         #region Methods
         public DateTime Date //Publiczna właściwość Date dla _date
