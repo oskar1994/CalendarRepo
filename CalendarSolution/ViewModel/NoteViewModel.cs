5acdd501270ff239bddc8b90d7f2d0e979b756b7
KaptejnSzyma <szymon.rzadca@gmail.com>
2017-05-15 00:17:19 +0200

    przeniesiono ICommandy w odpowiednie miejsce

--- CalendarSolution/ViewModel/NoteViewModel.cs
+++ CalendarSolution/ViewModel/NoteViewModel.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CalendarSolution.ViewModel
 {
     public class NoteViewModel
     {
+        private ICommand cancelButtonCommand;
+        private ICommand okButtonCommand;
+
+        private void OkButton()
+        {
+            throw new NotImplementedException();
+        }
+
         private void CancelButton()
         {
-
+            throw new NotImplementedException();
         }
+
+        public ICommand OkButtonCommand
+        {
+            get
+            {
+                okButtonCommand = new RelayCommand<object>((x) => OkButton());
+                return okButtonCommand;
+            }
+        }
+
+        public ICommand CancelButtonCommand
+        {
+            get
+            {
+                cancelButtonCommand = new RelayCommand<object>((x) => CancelButton());
+                return cancelButtonCommand;
+            }
+        }
+
     }
 }
